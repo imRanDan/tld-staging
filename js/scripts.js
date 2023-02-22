e713861d6475fe9d250b58ce64ab192628bb2cf1
Danyal Imran <dan.imran97@gmail.com>
2023-02-22 15:52:31 -0500

    finished hero/home section

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
-// When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+// When the user scrolls to the top of the page, slide up the navbar (200px out of the top view)
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.top = "-200px";
   } else {
     document.getElementById("navbar").style.top = "0";
